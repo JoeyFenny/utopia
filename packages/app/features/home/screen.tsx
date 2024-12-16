6ee155a4c08ff0e637220928f1f09b8db47f1ac1
Joey Fenny <joeyfenny@gmail.com>
2024-12-16 23:40:02 +0800

    hid the title bar

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,8 +1,5 @@
-import { A, H1, P, Text, TextLink } from 'app/design/typography'
-import { Row } from 'app/design/layout'
+import { H1 } from 'app/design/typography'
 import { View } from 'app/design/view'
-
-import { MotiLink } from 'solito/moti'
 
 export function HomeScreen() {
   return (
