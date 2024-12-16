a3f9902bc8059c9f5caa8627bdccf9e9811f56ad
Joey Fenny <joeyfenny@gmail.com>
2024-12-16 23:44:58 +0800

    made it dark

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -3,8 +3,8 @@ import { View } from 'app/design/view'
 
 export function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center p-3">
-      <H1>Utopia</H1>
+    <View className="flex-1 items-center justify-center p-3 bg-black">
+      <H1 className="text-white">Utopia</H1>
     </View>
   )
 }
