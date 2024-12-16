70b72111aedfd20bf44033775bc117e5bbe68e7b
Joey Fenny <joeyfenny@gmail.com>
2024-12-17 00:08:35 +0800

    added more text

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -5,6 +5,8 @@ export function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center p-3 bg-black">
       <H1 className="text-white">Utopia</H1>
+      <H1 className="text-white">Powered by</H1>
+      <H1 className="text-white">Utopia</H1>
     </View>
   )
 }
